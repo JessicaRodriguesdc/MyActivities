5a966211c1f2bbb744e88bff468cece8c8f699df
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-10-06 16:16:46 -0300

    finalizando a criacao do layout da tela de cadastro

--- app/src/main/java/br/edu/uni/si/myactivities/RegisterActivity.java
+++ app/src/main/java/br/edu/uni/si/myactivities/RegisterActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ImageButton ibVoltar;
+    private EditText ettNome;
+    private EditText ettEmail;
+    private EditText ettPassword;
+    private Button btLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,10 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         ibVoltar = (ImageButton) findViewById(R.id.ibVoltar);
+        ettNome = (EditText) findViewById(R.id.ettNome);
+        ettEmail = (EditText) findViewById(R.id.ettEmail);
+        ettPassword = (EditText) findViewById(R.id.ettPassword);
+        btLimpar = (Button) findViewById(R.id.btLimpar);
 
         ibVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +34,23 @@ public class RegisterActivity extends AppCompatActivity {
                 ibVoltar(v);
             }
         });
+        btLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btLimpar(v);
+            }
+        });
     }
 
     public void ibVoltar(View v){
-        Intent intent = new Intent(this,MenuActivity.class);
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+
+    public void btLimpar(View v){
+        ettNome.setText("");
+        ettEmail.setText("");
+        ettPassword.setText("");
+        ettNome.requestFocus();
     }
 }
