5ac694f7297eaa5ee814261c3f16913d06990823
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-10-09 16:19:47 -0300

    iniciando validacao e funcao de registrar

--- app/src/main/java/br/edu/uni/si/myactivities/RegisterActivity.java
+++ app/src/main/java/br/edu/uni/si/myactivities/RegisterActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+import br.edu.uni.si.myactivities.model.Pessoa;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText ettEmail;
     private EditText ettPassword;
     private Button btLimpar;
+    private Button btSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,8 @@ public class RegisterActivity extends AppCompatActivity {
         ettEmail = (EditText) findViewById(R.id.ettEmail);
         ettPassword = (EditText) findViewById(R.id.ettPassword);
         btLimpar = (Button) findViewById(R.id.btLimpar);
+        btSalvar = (Button) findViewById(R.id.btSalvar);
+
 
         ibVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +46,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 btLimpar(v);
+            }
+        });
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btSalvar(v);
             }
         });
     }
@@ -53,4 +67,34 @@ public class RegisterActivity extends AppCompatActivity {
         ettPassword.setText("");
         ettNome.requestFocus();
     }
+
+    public void btSalvar(View v){
+        if(ettNome.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Campo Nome vazio",Toast.LENGTH_LONG).show();
+            ettNome.requestFocus();
+            return;
+        }
+        if(ettEmail.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Campo Email vazio",Toast.LENGTH_LONG).show();
+            ettEmail.requestFocus();
+            return;
+        }
+        if(ettPassword.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Campo Senha vazio",Toast.LENGTH_LONG).show();
+            ettPassword.requestFocus();
+            return;
+        }
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome(ettNome.getText().toString());
+        pessoa.setEmail(ettEmail.getText().toString());
+        pessoa.setSenha(ettPassword.getText().toString());
+
+        if(!pessoa.toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Cadastro feito com sucesso! Bem vindo(a) "+pessoa.getNome(),Toast.LENGTH_LONG).show();
+            ibVoltar(v);
+        }
+
+    }
+
 }
