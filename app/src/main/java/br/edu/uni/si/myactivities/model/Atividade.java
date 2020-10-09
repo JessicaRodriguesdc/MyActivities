348a6b3e5f59a312d95a56cd4c064e646381daa8
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-10-09 16:16:40 -0300

    criando model´s

--- app/src/main/java/br/edu/uni/si/myactivities/model/Atividade.java
+++ app/src/main/java/br/edu/uni/si/myactivities/model/Atividade.java
@@ -1,4 +1,82 @@
 package br.edu.uni.si.myactivities.model;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Atividade {
+
+    private String nome;
+    private String descricao;
+    private Date dataInicial;
+    private Date dataFinal;
+
+    public Atividade() {
+    }
+
+    public Atividade(String nome, String descricao, Date dataInicial, Date dataFinal) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataInicial = dataInicial;
+        this.dataFinal = dataFinal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Atividade{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataInicial=" + dataInicial +
+                ", dataFinal=" + dataFinal +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(nome, atividade.nome) &&
+                Objects.equals(descricao, atividade.descricao) &&
+                Objects.equals(dataInicial, atividade.dataInicial) &&
+                Objects.equals(dataFinal, atividade.dataFinal);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal);
+    }
 }
--- app/src/main/java/br/edu/uni/si/myactivities/model/Pessoa.java
+++ app/src/main/java/br/edu/uni/si/myactivities/model/Pessoa.java
@@ -1,4 +1,67 @@
 package br.edu.uni.si.myactivities.model;
 
+import java.util.Objects;
+
 public class Pessoa {
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(nome, pessoa.nome) &&
+                Objects.equals(email, pessoa.email) &&
+                Objects.equals(senha, pessoa.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, email, senha);
+    }
 }
