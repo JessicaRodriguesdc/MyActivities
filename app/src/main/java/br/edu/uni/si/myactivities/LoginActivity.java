afd7581dd590ebdd3600f27eac126b83c6538def
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-11-15 20:05:05 -0300

    add metodo de logar no app  e o de conexao

--- app/src/main/java/br/edu/uni/si/myactivities/LoginActivity.java
+++ app/src/main/java/br/edu/uni/si/myactivities/LoginActivity.java
@@ -3,6 +3,8 @@ package br.edu.uni.si.myactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +12,16 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import br.edu.uni.si.myactivities.conn.DatabaseHelper;
+import br.edu.uni.si.myactivities.dao.AtividadeDAO;
+import br.edu.uni.si.myactivities.dao.PessoaDAO;
+import br.edu.uni.si.myactivities.model.Pessoa;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private SQLiteDatabase mConection;
+    private DatabaseHelper mDataHelper;
+    private PessoaDAO pessoaDAO;
     private ImageButton ibVoltar;
     private EditText ettLogin;
     private EditText ettPassword;;
@@ -21,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        createConection();
 
         ibVoltar = (ImageButton) findViewById(R.id.ibVoltar);
         ettLogin = (EditText) findViewById(R.id.ettLogin);
@@ -42,6 +53,16 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void createConection(){
+        try{
+            mDataHelper = new DatabaseHelper(this.getApplicationContext());
+            mConection = mDataHelper.getWritableDatabase();
+            Toast.makeText(this, "Banco criado com sucesso", Toast.LENGTH_SHORT).show();
+        }catch(SQLException e){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void ibVoltar(View v){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
@@ -49,6 +70,12 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void btLogin(View v){
+        this.pessoaDAO =  new PessoaDAO(mConection);
+
+        String login = ettLogin.getText().toString();
+        String senha = ettPassword.getText().toString();
+
+        Boolean exist = this.pessoaDAO.getLogar(login,senha);
 
         if(ettLogin.getText().toString().equals("")){
             Toast.makeText(getApplicationContext(),"Campo Login vazio",Toast.LENGTH_LONG).show();
@@ -58,6 +85,12 @@ public class LoginActivity extends AppCompatActivity {
         if(ettPassword.getText().toString().equals("")){
             Toast.makeText(getApplicationContext(),"Campo Senha vazio",Toast.LENGTH_LONG).show();
             ettPassword.requestFocus();
+            return;
+        }
+
+        if(exist == false){
+            Toast.makeText(getApplicationContext(),"Usuario ou Senha invalida",Toast.LENGTH_LONG).show();
+            ettLogin.requestFocus();
             return;
         }
 
